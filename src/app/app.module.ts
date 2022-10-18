2bc578a36584b6b782f247a3720c01628b9a9876
Alexandre Silva <alexandre.b.silva@capgemini.com>
2022-10-18 08:12:33 -0300

    Final meu-primeiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CursosModule } from './cursos/cursos.module';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.scss']
+})
+export class CursosComponent implements OnInit {
+
+  nomePortal: string ;
+  cursos: string[];
+
+  constructor(private cursoService : CursosService) {
+    this.nomePortal = 'http://loiane.training';
+    this.cursos = this.cursoService.getCursos();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
